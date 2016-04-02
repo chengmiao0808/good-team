ee4edcc8857e0ff913e4775eeba644648dc23546
Miao Cheng <Miao@SEAS214.wlan.seas.upenn.edu>
2016-04-02 16:09:24 -0400

    dchat.h updated

--- dchat.h
+++ dchat.h
@@ -16,22 +16,15 @@ using namespace std;
 class dchat
 {
 	int heartbeat_time;
-	int group_ids[100];
-	// Store all the members' info in the chat room
-	map<string, string> all_members_list; // <"ip_address:port", "group_id&name">
-	// Store all the members in certain groups
-	map<int, set<string>> group_members; // <group_id, set("ip_address:port")>
-	// Store the leader's info for each group
-	map<int, string> group_leaders; // <group_id, leader's "ip_address:port">
+	// Store all the members' info in the current group
+	map<string, string> all_members_list; // <"ip_address:port", "name">
+	// Store the leader's "ip_address:port" info
+	string leader;
 
 public:
 	dchat() {
 		int heartbeat_time = 5;
-		for (int i = 0; i < 100; i++)
-			group_ids[i] = 0;
 		all_members_list = map<string, string>();
-		group_members = map<int, set<string>>();
-		group_leaders = map<int, string>();
 	}
 	void start_new_group(string addr, string name);
 	void join_a_group(string add1, string add2, string name);
