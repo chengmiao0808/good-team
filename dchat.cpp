492dbb5b097d2c9ecaf6ec8813c92dea6df96207
Miao Cheng <Miao@SEAS306.wlan.seas.upenn.edu>
2016-04-07 13:29:59 -0400

    updated by miao

---
+++ dchat.cpp
@@ -0,0 +1,21 @@
+#include "dchat.h"
+#include "member.h"
+
+using namespace std;
+
+void error(char *err) {
+  perror(err);
+  exit(1);
+}
+
+void dchat::start_new_group(string l_addr, string l_name) {
+
+}
+
+void dchat::join_a_group(string l_addr, string m_addr, string m_name) {
+
+}
+
+int main(int argc, char *argv[]) {
+	
+}
--- dchat.h
+++ dchat.h
@@ -1,32 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sysexits.h>
-#include <signal.h>
-#include <string.h>
+#ifndef DCHAT_H
+#define DCHAT_H
+
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <string>
 #include <iostream>
 #include <map>
 #include <set>
-
-using namespace std;
+#include <vector>
+#include <deque>
+#include <sysexits.h>
+#include <signal.h>
 
 class dchat
 {
-	int heartbeat_time;
+public:
 	// Store all the members' info in the current group
 	map<string, string> all_members_list; // <"ip_address:port", "name">
 	// Store the leader's "ip_address:port" info
 	string leader;
+	bool is_leader;
 
-public:
 	dchat() {
-		int heartbeat_time = 5;
 		all_members_list = map<string, string>();
 	}
-	void start_new_group(string addr, string name);
-	void join_a_group(string add1, string add2, string name);
+	void start_new_group(string l_addr, string l_name);
+	void join_a_group(string l_addr, string m_addr, string m_name);
 	void elect_a_leader();
 };
+
+#endif
