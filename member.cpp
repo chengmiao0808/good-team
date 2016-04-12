971ee75e0e083dc6fddc918c971989e6a0ec8870
hanabeast <f331741897@gmail.com>
2016-04-11 22:02:35 -0400

    update

--- member.cpp
+++ member.cpp
@@ -84,7 +84,7 @@ int start_a_regular_member(dchat *p_chat, string l_addr, string m_addr, string m
 
     int currtime = getLocalTime();  //get current time with utility function
     msgpack msg_pack(ip_addr_me, atoi(portno_me), m_name, currtime, 1, "N/A");
-    string msg_sent = msgpack::serialize(msg_pack);
+    string msg_sent = utility::serialize(msg_pack);
     strcpy(buff, msg_sent.c_str());
 
     p_chat->num = sendto(p_chat->sock, buff, strlen(buff), 0, (struct sockaddr *) &(p_chat->other), sizeof(p_chat->other));
@@ -99,7 +99,7 @@ int start_a_regular_member(dchat *p_chat, string l_addr, string m_addr, string m
         return p_chat->num;
     }
     string msg_recv = buff;
-    msg_pack = msgpack::deserialize(msg_recv);
+    msg_pack = utility::deserialize(msg_recv);
     string members = msg_pack.msg;
     vector<string> vec = split(members, "\t");
     p_chat->leader = vec.pop_back();
