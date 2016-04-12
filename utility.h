85af58e2324a98f68ba2c017ee17716d28b687a1
Miao Cheng <leyi88cm@gmail.com>
2016-04-12 10:29:06 -0400

    updated

--- utility.h
+++ utility.h
@@ -1,47 +1,22 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
-
 #include <string>
 #include <stdio.h>      
 #include <time.h>      /* time_t, struct tm, difftime, time, mktime */
 #include <vector>
 #include <stdlib.h>
+#include "msgpack.h"
 
 using namespace std;
 
-class msgpack {
-public:
-	string IP;
-	int port;
-	string username;
-	int t_time;
-	int command;
-	//command:
-	//0: join
-	//1: leave
-	string msg;
-	
-	msgpack () {};
-
-	msgpack(string IPaddress, int portNum, string name, int timestamp, int cmd, string message) {
-		IP = IPaddress;
-		port = portNum;
-		username = name;
-		t_time = timestamp;
-		command = cmd;
-		msg = message;
-	} 
-    
-};
-
-
 int getLocalTime();
+
 string serialize(msgpack m);
+
 msgpack deserialize(string str);
+
 vector<string> split(string str, string sep);
-
-
 
 
 #endif
