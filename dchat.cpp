4d21098f42766031b4ecf81178294ec9baf9c4ce
Miao Cheng <leyi88cm@gmail.com>
2016-04-18 19:46:28 -0400

    updated

--- dchat.cpp
+++ dchat.cpp
@@ -13,10 +13,45 @@ void error(string err) {
   exit(1);
 }
 
-int start_a_leader(dchat *p_chat, string l_addr, string l_name) {
-  cout<<"start_a_leader is called!"<<endl;
-  cout<<l_addr<<endl;
-  vector<string> vec = split(l_addr, ":");
+string dchat::get_ip_address() {
+  struct ifaddrs *p_ifaddrs, *p_ifa;
+  int p_family, n;
+  char host[NI_MAXHOST];
+
+  if (getifaddrs(&p_ifaddrs) == -1) {
+    error("Error with getifaddrs!\n");
+  }
+
+  p_ifa = p_ifaddrs;
+  while(1) {
+    if (p_ifa == NULL)
+      break;
+    if (p_ifa->ifa_addr == NULL)
+      continue;
+    p_family = p_ifa->ifa_addr->sa_family;
+    if (p_family == AF_INET) {
+      n = getnameinfo(p_ifa->ifa_addr,
+            sizeof(struct sockaddr_in),
+            host, NI_MAXHOST,
+            NULL, 0, NI_NUMERICHOST);
+      if (n != 0) {
+        error("Error with getnameinfo!\n");
+      }
+      cout<<p_ifa->ifa_name<<endl;
+      cout<<"host is: "<<host<<endl;
+      if (strcmp(p_ifa->ifa_name, "en0") == 0) {
+        string my_ip = string(host);
+        freeifaddrs(p_ifaddrs);
+        cout<<"my_ip is: "<<my_ip<<endl;
+        return my_ip;
+      }
+    }
+    p_ifa = p_ifa->ifa_next;
+  }
+}
+
+int bind_socket(dchat *p_chat, string my_addr) {
+  vector<string> vec = split(my_addr, ":");
   string ip_addr = vec.front();
   string portno = vec.back();
   
@@ -43,20 +78,9 @@ int start_a_regular_member(dchat *p_chat, string l_addr, string m_addr, string m
   string ip_addr_me = vec_me.front();
   string portno_me = vec_me.back();
 
-  p_chat->sock = socket(AF_INET, SOCK_DGRAM, 0); 
-  if (p_chat->sock < 0) {
-    return p_chat->sock;
-  }
-
-  bzero((char *) &(p_chat->me), sizeof(p_chat->me)); 
-  p_chat->me.sin_family = AF_INET;
-  p_chat->me.sin_addr.s_addr = inet_addr(ip_addr_me.c_str());
-  p_chat->me.sin_port = htons(stoi(portno_me));
-
-  p_chat->sock2 = ::bind(p_chat->sock, (struct sockaddr *) &(p_chat->me), sizeof(p_chat->me));
-  if (p_chat->sock2 < 0) {
-    return p_chat->sock2;
-  }
+  int n = bind_socket(p_chat, m_addr);
+  if (n < 0)
+    return n;
 
   vector<string> vec_other = split(l_addr, ":");
   string ip_addr_other = vec_other.front();
@@ -104,43 +128,6 @@ int start_a_regular_member(dchat *p_chat, string l_addr, string m_addr, string m
   return 0;
 }
 
-string dchat::get_ip_address() {
-	struct ifaddrs *p_ifaddrs, *p_ifa;
-  int p_family, n;
-  char host[NI_MAXHOST];
-
-  if (getifaddrs(&p_ifaddrs) == -1) {
-    error("Error with getifaddrs!\n");
-  }
-
-  p_ifa = p_ifaddrs;
-  while(1) {
-  	if (p_ifa == NULL)
-  		break;
-  	if (p_ifa->ifa_addr == NULL)
-  		continue;
-  	p_family = p_ifa->ifa_addr->sa_family;
-  	if (p_family == AF_INET) {
-  		n = getnameinfo(p_ifa->ifa_addr,
-      			sizeof(struct sockaddr_in),
-            host, NI_MAXHOST,
-            NULL, 0, NI_NUMERICHOST);
-      if (n != 0) {
-      	error("Error with getnameinfo!\n");
-      }
-      cout<<p_ifa->ifa_name<<endl;
-      cout<<"host is: "<<host<<endl;
-      if (strcmp(p_ifa->ifa_name, "en0") == 0) {
-        string my_ip = string(host);
-        freeifaddrs(p_ifaddrs);
-        cout<<"my_ip is: "<<my_ip<<endl;
-        return my_ip;
-      }
-  	}
-  	p_ifa = p_ifa->ifa_next;
-  }
-}
-
 void dchat::start_new_group(string l_name) {
   cout<<"dchat::start_new_group is called!\n";
 	is_leader = true;
@@ -150,7 +137,7 @@ void dchat::start_new_group(string l_name) {
     srand((unsigned)time(NULL));
     int portno = rand() % 2000 + 8000;
     string l_addr = get_ip_address()+":"+to_string(portno);
-    int n = start_a_leader(this, l_addr, l_name);
+    int n = bind_socket(this, l_addr);
     if (n == 0) {
       leader = l_addr;
       all_members_list[l_addr] = l_name;
