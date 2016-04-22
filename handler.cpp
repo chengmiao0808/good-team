f4299c45e06d0449a7960ca15a844cd5b235f457
sblmt-lyp <youryi999@gmail.com>
2016-04-22 18:17:43 -0400

    Update handler.cpp

--- handler.cpp
+++ handler.cpp
@@ -167,21 +167,25 @@ command#$time_stamp#$client_ip:client:port (command is election)
 */
 void handle_election(dchat* p_chat, vector<string> message) {
   cout<<"election"<<message.size()<<endl;
+  if(p_chat->is_election == true) return;
 
+  p_chat->is_election = true;
+  bool is_winner = true;
   p_chat->all_members_list.erase(p_chat->leader_addr);
   p_chat->leader_addr = "";
 
   if(!message.empty()){
-    cout<<"here"<<endl;
+    cout<<"Others notice there is an new election"<<endl;
+
     // command#$time_stamp#$client_ip:client:port (command is election)    
     vector<string> vec = message;
     string cmd = vec[0];
-    // int msg_time = stoi(vec[1]); 
+    int msg_time = stoi(vec[1]); 
     string informer_addr =vec[2];
-    cout<<"here"<<endl;
 
     // I bully this informer, or I am the 1st one who start the election 
     if(bully_compare(p_chat->my_addr, informer_addr) ){
+
       // 1. send back "okay": command#$time_stamp#$client_ip:client_port. (command is refuse)
       string okay_msg = "refuse#$" + to_string(p_chat->current_stamp) + "#$" +p_chat->my_addr;
       send_handler(okay_msg, informer_addr, p_chat);
@@ -196,14 +200,18 @@ void handle_election(dchat* p_chat, vector<string> message) {
       }
 
     }else{ 
-      /* election loser, impossible to have this condition */
+      /*  Others inform you as an election loser
+          Impossible to have this condition 
+      */
     }
 
-  }else{ // start a new election
+  }else{ // Input vector.size() = 0   : FOUND LEADER DEAD & START A NEW ELECTION
 
-      // 1. send election to others with higher priority: 
-      //    command#$time_stamp#$client_ip:client:port (command is election)
+      /* 1. send election to others with higher priority: 
+          command#$time_stamp#$client_ip:client:port (command is election)
+      */
       string elect_msg = "election#$" + to_string(p_chat->current_stamp) + "#$" + p_chat->my_addr;
+      cout<<elect_msg<<endl;
       for (auto iter = p_chat->all_members_list.begin(); iter != p_chat->all_members_list.end(); iter++) {
         if( !bully_compare(p_chat->my_addr, iter->first) && p_chat->my_addr != iter->first){
           cout<<"send to:\t"<<iter->first<<iter->second<<endl;
@@ -212,47 +220,92 @@ void handle_election(dchat* p_chat, vector<string> message) {
       }    
   }
 
-  	// 3. wait for the result
-  	int start_time = getLocalTime();
-  	while( getLocalTime() - start_time < 5){
-	    char buff[2048];
-	    bzero(buff, 2048);
-	    p_chat->num = recvfrom(p_chat->sock, buff, 2048, 0, (struct sockaddr *) &(p_chat->other), (socklen_t *) &(p_chat->len));
-	    if (p_chat-> num < 0) {
-	      cout<<"Error with recvfrom!"<<endl;
-	    }
-	    vector<string> message = split(buff);
-  		if (message[0].compare("refuse") == 0) {
-    	  p_chat->is_leader = false;
-  		} 
+  	// 3. wait for others' messages
+    int count = 3;
+  	while( count > 0){
+      count--;
+
+      //Receive message from others
+      struct timeval tv;
+      tv.tv_sec = 1;
+      tv.tv_usec = 0;
+      if (setsockopt(p_chat->sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        error("Recvfrom timeout!\n");
+      }
+
+      cout<<"here0"<<endl;
+      
+      char temp[2048];
+      bzero(temp, 2048);
+      p_chat->num = recvfrom(p_chat->sock, temp, 2048, 0, (struct sockaddr *) &(p_chat->other), (socklen_t *) &(p_chat->len));
+      cout<<"here0.1"<<endl;
+      if (p_chat-> num < 0) {
+        error("Error with recvfrom!\n");
+      }
+
+      cout<<"here1"<<endl;
+
+      tv.tv_sec = 99999999;
+      if (setsockopt(p_chat->sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        error("Recvfrom timeout!\n");
+      }
+
+      cout<<"here2"<<endl;
+
+      cout<<"election recvfrom: \t"<< temp<<endl;
+	    if(temp[0] != '/0'){
+        vector<string> message = split(temp);
+    		if (message[0].compare("refuse") == 0) {// refuse message
+      	  is_winner = false;
+    		}else{ //other unexpected message
+        
+        }
+        count++;
+      }
+
   	}
-  	// 4. If you win, broadcast it!
-  	// new_leader_create: when a new leader is elected, 
-  	// we would multicast the msg to every client. 
-  	// command#$time_stamp#$leader_ip:leader_port#$leader_name. (command is new_leader) 
-    p_chat->is_leader = true;
-  	if(p_chat->is_leader){
-  	  p_chat->leader_addr = p_chat->my_addr;
-  	  p_chat->leader_stamp = p_chat->current_stamp;
-  	  string winner_line = "new_leader#$" + to_string(p_chat->current_stamp) + "#$" + p_chat->my_addr + "#$" +p_chat->my_name;
-	  cout<<"Notice "<< " new leader "<<p_chat-> my_name<<" is hearing on "<<p_chat-> my_addr<<endl; 
-  	  broadcast(p_chat, winner_line);	
-  	}
+
+  	/* 4. If you win, broadcast it!
+  	   new_leader_create: when a new leader is elected,  we would multicast the msg to every client. 
+  	   command#$time_stamp#$leader_ip:leader_port#$leader_name. (command is new_leader) 
+    */
+    if(is_winner == true){
+      cout<<"You win!"<<endl;
+      p_chat->is_election = false;
+
+      /************TO DO: Initiate the status as a leader ****************/
+      p_chat->is_leader = true;
+    	p_chat->leader_addr = p_chat->my_addr;
+    	p_chat->leader_stamp = p_chat->current_stamp; // not sure about this?
+      /************TO DO: Initiate the status as a leader ****************/
+
+    	string winner_line = "new_leader#$" + to_string(p_chat->leader_stamp) + "#$" + p_chat->my_addr + "#$" +p_chat->my_name;
+  	  cout<<"Notice "<< " new leader "<<p_chat-> my_name<<" is hearing on "<<p_chat-> my_addr<<endl; 
+      broadcast(p_chat, winner_line);	
+
+    }else{
+      return;
+    }  	
 
 }
 
-/*	new_leader_create: when a new leader is elected, 
-	we would multicast the msg to every client. 
+/*	new_leader_create: when a new leader is elected, we would multicast the msg to every client. 
 	command#$time_stamp#$leader_ip:leader_port#$leader_name. (command is new_leader)
 */
 void handle_new_leader(dchat* p_chat,vector<string> message){
+
+  p_chat->is_election = false;
+
+/************TO DO: Initiate the status for a new leader ****************/
   vector<string> vec = message;
   string cmd = vec[0];
-  // int msg_time = stoi(vec[1]); 
+  p_chat->leader_stamp = stoi(vec[1]); // not sure about this?
   string new_leader_addr =vec[2];
   string new_leader_name = vec[3];
-  p_chat->leader_addr = new_leader_addr;
 
+
+  p_chat->leader_addr = new_leader_addr;
+/************TO DO: Initiate the status for a new leader ****************/
   cout<<"NOTICE new leader "<<new_leader_name<<" is listening on "<<new_leader_addr<<endl; 
 }
 
